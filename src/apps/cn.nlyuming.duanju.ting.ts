e1298057565ab5230926cc7790711a4f71a025c6
jiuqianyuan <39406781+jiuqianyuan@users.noreply.github.com>
2024-11-25 20:16:54 +0800

    Update cn.nlyuming.duanju.ting.ts

--- src/apps/cn.nlyuming.duanju.ting.ts
+++ src/apps/cn.nlyuming.duanju.ting.ts
@@ -10,7 +10,6 @@ export default defineGkdApp({
       desc: '点击关闭',
       rules: [
         {
-          activityIds: 'cn.nlyuming.duanju.ting.MainActivity',
           matches:
             '[id="android:id/content"] >5 FrameLayout[childCount=3] >2 ImageView[visibleToUser=true]',
           exampleUrls:
